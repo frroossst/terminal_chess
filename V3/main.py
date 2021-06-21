3cd1fb0de891bd48675ff4e61bdc26dcab35390a
frroossst <adhyanpatel@gmail.com>
2021-06-21 20:06:06 +0530

    3.1.0 - Board layour is now a class variable

--- V3/main.py
+++ V3/main.py
@@ -33,6 +33,28 @@ class Pieces():
     b_pawn = "♟" 
 
 class Board(Pieces):
+
+    li= [[Pieces.b_rooke,Pieces.b_knight,Pieces.b_bishop,Pieces.b_queen,Pieces.b_king,Pieces.b_bishop,Pieces.b_knight,Pieces.b_rooke,"8"],
+             [Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,"7"],
+             [" "," "," "," "," "," "," "," ","6"],
+             [" "," "," "," "," "," "," "," ","5"],   
+             [" "," "," "," "," "," "," "," ","4"],
+             [" "," "," "," "," "," "," "," ","3"],
+             [Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,"2"],
+             [Pieces.w_rooke,Pieces.w_knight,Pieces.w_bishop,Pieces.w_queen,Pieces.w_king,Pieces.w_bishop,Pieces.w_knight,Pieces.w_rooke,"1"],
+        ]
+
+    li_ref = [
+            [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],
+            [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],
+            [(0,5),(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5)],
+            [(0,4),(1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,4)],
+            [(0,3),(1,3),(2,3),(3,3),(4,3),(5,3),(6,3),(7,3)],
+            [(0,2),(1,2),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2)],
+            [(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)],
+            [(0,0),(0,1),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
+        ]
+
     def __init__(self):
         mycursor.execute("drop table board")
         mycursor.execute("create table board (Location char(5), Piece varchar(15), Colour char(5));")
@@ -76,34 +98,15 @@ class Board(Pieces):
         db.commit()
 
     def create_board(self):
-        global li
-        li= [[Pieces.b_rooke,Pieces.b_knight,Pieces.b_bishop,Pieces.b_queen,Pieces.b_king,Pieces.b_bishop,Pieces.b_knight,Pieces.b_rooke,"8"],
-             [Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,Pieces.b_pawn,"7"],
-             [" "," "," "," "," "," "," "," ","6"],
-             [" "," "," "," "," "," "," "," ","5"],   
-             [" "," "," "," "," "," "," "," ","4"],
-             [" "," "," "," "," "," "," "," ","3"],
-             [Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,Pieces.w_pawn,"2"],
-             [Pieces.w_rooke,Pieces.w_knight,Pieces.w_bishop,Pieces.w_queen,Pieces.w_king,Pieces.w_bishop,Pieces.w_knight,Pieces.w_rooke,"1"],
-        ]
-
-        li_ref = [
-            [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7)],
-            [(0,6),(1,6),(2,6),(3,6),(4,6),(5,6),(6,6),(7,6)],
-            [(0,5),(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5)],
-            [(0,4),(1,4),(2,4),(3,4),(4,4),(5,4),(6,4),(7,4)],
-            [(0,3),(1,3),(2,3),(3,3),(4,3),(5,3),(6,3),(7,3)],
-            [(0,2),(1,2),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2)],
-            [(0,1),(1,1),(2,1),(3,1),(4,1),(5,1),(6,1),(7,1)],
-            [(0,0),(0,1),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0)],
-        ]
-        for i in li:
+        
+        
+        for i in Board.li:
             print(i)
         label = ["a","b","c","d","e","f","g","h"]
         print(label)
         logging.debug("starting position board printed")
 
-    def update_board(self,piece,prev_loc,now_loc):
+    def update_board(self,piece,prev_loc,now_loc,):
         self.piece = piece
         self.prev_loc = prev_loc
         self.now_loc = now_loc
@@ -121,12 +124,21 @@ class Board(Pieces):
         B.show_updated_board()
         
     def show_updated_board(self):
+        self.li = Board.li
         loc_dict_local = Movement.loc_dict
         mycursor.execute("select * from board;")
         result = mycursor.fetchall()
         # create a method to query through table board and reconstruct a board as a list
 
-
+    def check_game_over(self,li):
+        if Pieces.w_king not in self.li:
+            print("White King has been captured!")
+            print("Black Wins!")
+        elif Pieces.b_king not in self.li:
+            print("Black King has been captured!")                
+            print("White Wins!")
+        else:
+            raise Exception ("ERROR_KING_NOT_FOUND_EITHER")
 
 class Movement():
         hor = ["a","b","c","d","e","f","g","h"]
