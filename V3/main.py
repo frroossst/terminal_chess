beee28c3c78e78d5b0a002e62124667e84455a16
frroossst <adhyanpatel@gmail.com>
2021-07-05 17:08:20 +0530

    castling RESTART

--- V3/main.py
+++ V3/main.py
@@ -55,9 +55,9 @@ mycursor.execute("create table board (Location char(5), Piece varchar(15), Colou
 logging.debug("white pieces data entry")
 mycursor.execute("insert into board values ('d1','Queen','White',NULL);")
 mycursor.execute("insert into board values ('e1','King','White',NULL);")
-# mycursor.execute("insert into board values ('f1','Bishop','White',NULL);")
+mycursor.execute("insert into board values ('f1','Bishop','White',NULL);")
 mycursor.execute("insert into board values ('c1','Bishop','White',NULL);")
-# mycursor.execute("insert into board values ('g1','Knight','White','g1');")
+mycursor.execute("insert into board values ('g1','Knight','White','g1');")
 mycursor.execute("insert into board values ('b1','Knight','White','b1');")
 mycursor.execute("insert into board values ('h1','Rook','White','h1');")
 mycursor.execute("insert into board values ('a1','Rook','White','a1');")
@@ -735,15 +735,8 @@ class Board(Pieces):
             if use_colour == "White":
                 w_inCheck = True
             elif use_colour == "Black":
-                b_inCheck = True
+                b_inCheck = True   
         
-        
-        
-    #to check whether castling is allowed or not
-        
-        
-        
-
     def draw_game(self,turn):
         self.turn = turn
         if ((self.turn-1) % 2) != 0:
@@ -1360,75 +1353,50 @@ class Movement():
                 quit()
 
         def check_castle(self,move):
-            self.move = move
-            wsqrS = ["f1","g1"]
-            bsqrS = ["f8","g8"]
-            wsqrL = ["d1","c1","b1"]
-            bsqrL = ["d8","c8","b8"]
-            castleable0 = True
-            castleable1 = True
-            castleable2 = True
+            B = Board()
+            B.incheck()
+            
+            castlePieceMoved = True # returns False if either the King or Rook was moved
+            castlePieceOccupied = True # return False if the castle squares are occupied 
 
             if ((turn-1) % 2) != 0:
                 turn_colour = "White"
             else:
                 turn_colour = "Black"
-
-            query0 = "select * from board where Location = '%s';"
-
-            if self.move == "O-O":
+            
+            if move == "O-O":
                 if turn_colour == "White":
-                    for i in wsqrS:
-                        mycursor.execute(query0 % i)
-                        result0 = mycursor.fetchall()
-                        if result0 != []:
-                            castleable0 = False
-                            break
-                    query1 = "select Moved from castle where Location in ('e1','h1');"
-                    mycursor.execute(query1)
-                    result1 = mycursor.fetchall()
-                    for i in result1:
-                        if i[0] == "n":
+                    # checking whether the King or Rook was moved before
+                    query = "select Moved from castle where Location in ('h1','e1');"
+                    mycursor.execute(query)
+                    result = mycursor.fetchall()
+                    for i in result:
+                        if i[0] == 'n':
                             pass
                         else:
-                            castleable1 = False
-                    if incheck_status != True:
-                        counter = 0
-                        while True:
-                            query2 = "update board set Location = '%s' where Piece = 'King' and Colour = 'White';"
-                            mycursor.execute(query2 % wsqrS[counter])
-                            db.commit()
-                            counter += 1
-                            if counter > 1:
-                                break
-                            Board.incheck(self)
-                            if incheck_status == False:
-                                castleable2 = True
-                            else:
-                                castleable2 = False
-                                break
+                            castlePieceMoved = False
+                    # checking wheather the pieces are occupied or not for the short castle
+                    OOsquaresWhite = ["f1","g1"]
+                    for i in OOsquaresWhite:
+                        query = "select * from board where Location ='%s';"
+                        mycursor.execute(query % i)
+                        result = mycursor.fetchall()
+                        if result != []:
+                            castlePieceOccupied = True
+                            break
+                        else:
+                            pass
 
-                # elif turn_colour == "Black":
-                #     for j in bsqrS:
-                #         mycursor.execute(query0 % j)
-                #         result0 = mycursor.fetchall()
-                #         if result0 != []:
-                #             castleable = False
-                #             break
-                #         query1 = "select Moved from castle where Location in ('e8','h8');"
-                #         mycursor.execute(query1)
-                #         result1 = mycursor.fetchall()
-                #         for i in result1:
-                #             if i[0] == "n":
-                #                 pass
-                #             else:
-                #                 castleable = False
-        
-            if castleable0 and castleable1 and castleable1:
-                print("[CASTLE STATUS] True")
+                elif turn_colour == "Black":
+                    pass
+            elif move == "O-O-O":
+                pass
+
+            if castlePieceOccupied and castlePieceMoved:
+                    print("[CAN CASTLE] True")
             else:
-                print("[CASTLE STATUS] False")
-                  
+                print("[CAN CASTLE] False")
+            
         # ~Check if the squares are occupied~
         # ~Check if the King was moved~
         # ~Check if the Rook was moved~
